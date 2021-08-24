1cee6efc222386975684251d49f2c772969326af
nichaos2 <nick.ioannou86@gmail.com>
2021-08-24 20:35:47 +0200

    add better view and characteristics in admin page

--- blog/admin.py
+++ blog/admin.py
@@ -8,4 +8,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Diplays the post a bit better than the default
+    """
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)} # creates the slug content based on the title
+
+admin.site.register(Post, PostAdmin)
