5a0f8e4a6bdf9b239d67f658f3e4fbfc586fd382
nichaos2 <nick.ioannou86@gmail.com>
2021-08-24 20:18:40 +0200

    create model for blog + add in admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
+"""
+Adding Models To The Administration Site
+Code copied from the site https://djangocentral.com/building-a-blog-application-with-django/
+"""
+
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post 
+
+admin.site.register(Post)
